a7512a63550c509cc9c8e815902d666c749a5521
Michael Schonfeld <michael@dwolla.com>
2012-06-03 08:37:53 -0500

    modal fix

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -3,5 +3,6 @@ $(".modal .close").live('click', function() {
 });
 
 $(".status.accepted span").click(function() {
-	$(this).parent().parent().find(".modal").fadeIn();
+	$(".modal").fadeIn();
 });
+
